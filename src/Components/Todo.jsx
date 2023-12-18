3265d02787f3d48265219921aad2d2d53896272b
gui <guiguidonega@gmail.com>
2023-12-18 16:03:46 -0300

    first

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+
+function Todo() {
+    const [edit, setedit] = useState({
+        id: null,
+        value: ''
+    })
+  return todos.map((todo, index) => (
+    <div></div>
+  ))
+}
+
+export default Todo
